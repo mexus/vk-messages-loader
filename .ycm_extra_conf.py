b133ea1fd0dbb7ed45b13c5af01f7287069556ee
Mexus <gilaldpellaeon@gmail.com>
2016-01-16 23:17:30 +0300

    Replaced `ycm` dir with a git submodule

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.normpath(os.path.join(here, 'ycm')))
 import ycmflags
 
 def FlagsForFile( filename ):
-    flags = ycmflags.YcmFlags([], enable_qt = False)
+    flags = ycmflags.YcmFlags()
     return flags.flags_for_file(filename)
