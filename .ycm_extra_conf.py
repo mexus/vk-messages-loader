ba4f8658eed0497850787ef9f903f2fcd0ce9693
Mexus <gilaldpellaeon@gmail.com>
2016-01-17 02:06:02 +0300

    Code completion improvments

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -6,5 +6,5 @@ sys.path.insert(0, os.path.normpath(os.path.join(here, 'ycm')))
 import ycmflags
 
 def FlagsForFile( filename ):
-    flags = ycmflags.YcmFlags()
+    flags = ycmflags.YcmFlags(default_file = ["storage/src/exceptions.cpp", []])
     return flags.flags_for_file(filename)
