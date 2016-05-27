88bfa83dd66e8cd53f5ce4b51cb282838aa3a251
Mexus <gilaldpellaeon@gmail.com>
2016-05-28 02:13:54 +0300

    Updated the `ycmflags` submodule

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,10 +1,11 @@
 import sys, os
 
-here = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.normpath(os.path.join(here, 'ycm')))
+base_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.normpath(os.path.join(base_path, 'ycm')))
 
 import ycmflags
 
 def FlagsForFile( filename ):
-    flags = ycmflags.YcmFlags(default_file = ["storage/src/exceptions.cpp", []])
+    flags = ycmflags.YcmFlags(default_file = ["storage/src/exceptions.cpp", []],
+            absolute_project_path = base_path)
     return flags.flags_for_file(filename)
